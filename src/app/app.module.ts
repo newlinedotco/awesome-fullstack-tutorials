8ace2504dbf29e2daec5b315d9063ba30016a9d0
Martin Jakubik <martin@jakubik.info>
2018-06-16 12:00:47 +0200

    add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './components/root/root.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ActionComponent } from './components/action/action.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpClientModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    ],
     declarations: [AppComponent, HeaderComponent, ActionComponent],
     bootstrap: [AppComponent],
 })
